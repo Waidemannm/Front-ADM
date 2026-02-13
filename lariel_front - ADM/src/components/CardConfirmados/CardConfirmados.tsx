c8600e0af0ff215c200da0dbd068229c0c73d382
Waidemannm <mjrmolinillo@icloud.com>
2026-02-13 19:44:43 -0300

    Add CardConfirmados component for displaying confirmed guests with update functionality

--- lariel_front - ADM/src/components/CardConfirmados/CardConfirmados.tsx
+++ lariel_front - ADM/src/components/CardConfirmados/CardConfirmados.tsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import type { TipoConvidado } from "../../types/tipoConvidado";
-import ConvidadoAtualizar from "../ConvidadoAtualizar/ConvidadoAtualizar";
-import ConvidadoDeletar from "../ConvidadoDeletar/ConvidadoDeletar";
+import ConfirmadosAtualizar from "../ConfirmadosAtualizar/ConfirmadosAtualizar";
 
-export default function CardConvidado(props: {convidado: TipoConvidado}){
+export default function CardConfirmados(props: {convidado: TipoConvidado}){
 
     const [openConvidadoAtualizar, setOpenConvidadoAtualizar] = useState(false);
-    const [openConvidadoDeletar, setConvidadoOpenDeletar] = useState(false);
+
 
     return(
-        <div className="text-white bg-gradient-to-br from-[var(--color-2)] via-blue-300 to-[var(--color-2)] flex flex-col items-center rounded-lg border-2 border-[var(--color-2)] p-6">
+        <div className="text-white bg-gradient-to-br from-[var(--color-2)] via-blue-300 to-[var(--color-2)] flex flex-col items-center rounded-lg border-2 border-[var(--color-2)] p-6 ">
             <h1 className="text-2xl font-medium">{props.convidado.nomeConvidado}</h1>
             <div className="m-2">
                 <p className="text-[var(--color-2)] text-sm"> Status: <span className="text-white font-medium">{props.convidado.status}</span></p>
@@ -20,12 +19,7 @@ export default function CardConvidado(props: {convidado: TipoConvidado}){
             <div className="text-[var(--color-2)] mt-10 flex justify-center gap-5 md:gap-8 mt-5">
                     <button className="cursor-pointer rounded-xl border-2 border-[var(--color-2)] font-medium w-20 md:w-25 h-13 md:h-10" onClick={() => setOpenConvidadoAtualizar(true)}>Atualizar</button>
 
-                    <ConvidadoAtualizar open={openConvidadoAtualizar} onClose={() => setOpenConvidadoAtualizar(false)}> </ConvidadoAtualizar>
-                    
-                    <button className="bg-red-400 cursor-pointer rounded-xl border-2 border-red-400 font-medium w-20 md:w-25 h-13 md:h-10" onClick={() => setConvidadoOpenDeletar(true)}>Deletar</button>
-
-                    <ConvidadoDeletar open={openConvidadoDeletar} onClose={() => setConvidadoOpenDeletar(false)}> </ConvidadoDeletar>
-
+                    <ConfirmadosAtualizar open={openConvidadoAtualizar} onClose={() => setOpenConvidadoAtualizar(false)}> </ConfirmadosAtualizar>
             </div>
         </div>
     );
