349139df3c65d91f0f9b8876118873b98baee9b4
Waidemannm <mjrmolinillo@icloud.com>
2026-02-14 13:22:25 -0300

    Fix image height in CardConvite component for better responsiveness

--- lariel_front - ADM/src/components/CardConvite/CardConvite.tsx
+++ lariel_front - ADM/src/components/CardConvite/CardConvite.tsx
@@ -8,7 +8,7 @@ export default function CardConvite(props: {convite: TipoConvite}){
     return(
         <div className="text-white bg-gradient-to-br from-[var(--color-2)] via-blue-300 to-[var(--color-2)] hover:scale-105 active:scale-95 transition-all duration-300 cursor-pointer rounded-lg border-2 border-[var(--color-2)] hover:bg-gradient-to-br hover:from-[var(--color-3)] hover:via-blue-300 hover:to-[var(--color-3)] p-6">
             <div className="flex flex-col-reverse md:flex-row justify-between items-center">
-                <img src={logo}alt="Logo Larissa e Samuel" className="w-15 h-15 md:w-40 md:h-40" />
+                <img src={logo}alt="Logo Larissa e Samuel" className="w-15 h-auto md:w-40" />
                 <div className="flex flex-col items-center">
                     <h1 className="convite-nome">{props.convite.nomeConvite}</h1>
                     <p className="text-sm">e Família</p>
